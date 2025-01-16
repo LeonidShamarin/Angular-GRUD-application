c3a5e57b81ef3f58c28b519330c6ab561e642112
Leonid <Leonideko1@gmail.com>
2025-01-16 23:27:17 +0200

    fix app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { MatToolbarModule } from '@angular/material/toolbar'; // Імпортуємо MatToolbarModule
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // Імпортуємо BrowserAnimationsModule
+
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, MatToolbarModule, BrowserAnimationsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
